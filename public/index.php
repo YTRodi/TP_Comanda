97efb01b4fe0739926f1ee01919373d77ab0c756
YTRodi <yagorodi@gmail.com>
2020-11-28 18:20:50 -0300

    feat: faltan encuestas

--- public/index.php
+++ public/index.php
@@ -79,41 +79,6 @@ $app->group( '/mesas', function ( RouteCollectorProxy $group ) {
 
 })->add( new JsonMiddleware );
 
-// $app->group( '/users', function ( RouteCollectorProxy $group ) {
-
-//     // Traer todos
-//     $group->get( '[/]', UsuarioController::class . ':getAllUsers' );
-
-//     // Punto 1
-//     $group->post( '[/]', UsuarioController::class . ':addUser' );
-
-// })->add( new JsonMiddleware );
-
-
-// // Punto 2
-// $app->post( '/login[/]', UsuarioController::class . ':loginUser' );
-
-
-// // Punto 3
-// $app->post( '/materia[/]', MateriaController::class . ':addMateria' )
-//     ->add( new AuthMiddleware( 'admin' ) )
-//     ->add( new JsonMiddleware );
-
-// // Punto 4
-// $app->post( '/inscripcion/{id}', InscripcionController::class . ':addInscripcion' )
-//     ->add( new AuthMiddleware( 'alumno' ) );
-
-
-// // Punto 5
-// $app->put( '/notas/{id}', InscripcionController::class . ':addNotaAlumno' )
-//     ->add( new AuthMiddleware( 'profesor' ) );
-
-
-// // Punto 7
-// $app->get( '/materia[/]', MateriaController::class . ':getAllMaterias' )
-//     ->add( new JsonMiddleware );
-
-
 
 $app->addBodyParsingMiddleware(); // Para poder usar los datos que enviamos desde el body para el PUT ( vamos por 'x-www-form-urlencoded', no por form-data)
 $app->run();
