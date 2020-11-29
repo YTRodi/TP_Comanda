91708a772ad42e17ae018bd715b39db7c8031864
YTRodi <yagorodi@gmail.com>
2020-11-29 17:54:35 -0300

    lelele

--- src/controllers/PreparacioneController.php
+++ src/controllers/PreparacioneController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controllers;
 
-use App\Models\Pedido;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 use App\Models\Preparacione;
-use Exception;
 
 class PreparacioneController {
 
