c0766fabd6047e22b5c8f848f3c5f28aa051f1cc
YTRodi <yagorodi@gmail.com>
2020-11-29 02:40:21 -0300

    prueba 99999

--- public/index.php
+++ public/index.php
@@ -1,39 +1,207 @@
 <?php
 
-// use Illuminate\Database\Capsule\Manager as Capsule;
-// use Illuminate\Events\Dispatcher;
-// use Illuminate\Container\Container;
+use App\Models\Usuario;
 
-// class Database {
+class UsuarioController {
+
+    // JWT
+    public function loginUser ( Request $request, Response $response ) {
+
+        //Username, password y sector
+        $body = $request->getParsedBody();
+        $username = $body['username'];
+        $password = $body['password'];
+        $sector = $body['sector'];
+
+        $user = Usuario::where('username','=',$username)
+                       ->where('password','=',$password)
+                       ->where('sector','=',$sector)
+                       ->first();
+
+        if ( $user ) {
+
+                $payload = [
+                    'id' => $user['id'],
+                    'username' => $user['username'],
+                    'password' => $user['password'],
+                    'sector' => $user['sector']
+                ];
+
+                $token = AuthJWT::Login( $payload );
+
+                // TOKEN!!!
+                print_r( $token );
+
+        } else {
+
+            $response->getBody()->write( 'No existe el usuario.' );
+
+        }
+
+        return $response;
+
+    }
+
+    // -------------------------------------------------------------------
+    // -------------------------------------------------------------------
+    // -------------------------------------------------------------------
+
+    // Funciones CRUD
+    public function getAllUsers ( Request $request, Response $response ) {
     
-//     public function __construct() {
+        $rta = Usuario::get();     
 
-//         $capsule = new Capsule;
+        $response->getBody()->write( json_encode( $rta ) );
 
-//         $capsule->addConnection([
-//             'driver'    => 'mysql',
-//             'host'      => 'bnqbjabqekwmoupl8t93-mysql.services.clever-cloud.com',
-//             'database'  => 'bnqbjabqekwmoupl8t93',
-//             'username'  => 'u3u8mcep1vn0irkg',
-//             'password'  => 'tz4BQqi9l0TOm7A6peqE',
-//             'charset'   => 'utf8',
-//             'collation' => 'utf8_unicode_ci',
-//             'prefix'    => '',
-//         ]);
+        return $response;
 
-//         // Set the event dispatcher used by Eloquent models... (optional)
+    }
 
-//         $capsule->setEventDispatcher(new Dispatcher(new Container));
+    public function getOneUser ( Request $request, Response $response, $args ) {
+    
+        $rta = Usuario::find( $args['id'] ); // pq no use el int val?
 
-//         // Make this Capsule instance available globally via static methods... (optional)
-//         $capsule->setAsGlobal();
+        $response->getBody()->write( json_encode( $rta ) );
 
-//         // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-//         $capsule->bootEloquent();
+        return $response;
         
-//     }
+    }
 
-// }
+    public function addUser ( Request $request, Response $response ) {
+
+        $user = new Usuario;
+        $user['username'] = $request->getParsedBody()['username'] ?? '';
+        $user['password'] = $request->getParsedBody()['password'] ?? '';
+        $user['estado'] = $request->getParsedBody()['estado'] ?? '';
+        $user['sector'] = $request->getParsedBody()['sector'] ?? '';
+        $user['operaciones'] = $request->getParsedBody()['operaciones'] ?? '';
+        // echo json_encode($user);
+
+        $rta = $user->save();
+        $response->getBody()->write( json_encode( $rta ) );
+
+        return $response;
+        
+    }
+
+    //! PARA CAMBIAR EL ESTADO! ( SÓLO ADMIN )
+    // public function updateUser ( Request $request, Response $response, $args ) {
+    
+    //     try {
+            
+    //         $idUrl = $args['id'] ?? '';
+    //         $user = Usuario::find( intval( $idUrl ) );
+
+    //         $rta = $user->save();
+    //         $response->getBody()->write( json_encode( $rta ) );
+    //         return $response;
+
+    //     } catch (\Throwable $e) {
+
+    //         throw new Exception( $e->getMessage() );
+
+    //     }
+        
+    // }
+
+    public function deleteUser ( Request $request, Response $response, $args ) {
+    
+        $idUrl = $args['id'] ?? '';
+        $user = Usuario::find( intval( $idUrl ) );
+        // echo json_encode($user);
+
+        if( $user ) {
+
+            $rta = $user->delete();
+            $response->getBody()->write( json_encode( $rta ) );
+            
+        } else {
+            $response->getBody()->write( 'No existe usuario con el id = ' . $idUrl );
+        }
+
+        return $response;
+        
+    }
+
+    // -------------------------------------------------------------------
+    // -------------------------------------------------------------------
+    // -------------------------------------------------------------------
+
+    // Funciones CRUD
+    // public function getAllUsers ( Request $request, Response $response ) {
+    
+    //     $rta = Usuario::get();     
+
+    //     $response->getBody()->write( json_encode( $rta ) );
+
+    //     return $response;
+
+    // }
+
+    // public function getOneUser ( Request $request, Response $response, $args ) {
+    
+    //     $rta = Usuario::find( $args['id'] );
+
+    //     $response->getBody()->write( json_encode( $rta ) );
+
+    //     return $response;
+        
+    // }
+
+    // public function addUser ( Request $request, Response $response ) {
+
+    //     // var_dump($request->getParsedBody());
+
+    //     $user = new Usuario;
+    //     $user->clave = $request->getParsedBody()['clave'] ?? '';
+    //     $user->email = $request->getParsedBody()['email'] ?? '';
+    //     $user->tipo = $request->getParsedBody()['tipo'] ?? '';
+    //     // echo $user->$clave . '<br/>';
+    //     // echo $user->$email . '<br/>';
+    //     // echo $user->$tipo . '<br/>';
+
+    //     $rta = $user->save();
+    //     $response->getBody()->write( json_encode( $rta ) );
+
+    //     return $response;
+        
+    // }
+
+    // public function updateUser ( Request $request, Response $response, $args ) {
+    
+    //     // var_dump( $args['id'] );
+    //     $idUrl = $args['id'] ?? '';
+    //     $user = Usuario::find( intval( $idUrl ) );
+    //     // echo json_encode( $user );
+
+    //     // Hago las modicaciones y vuelvo a guardar
+    //     $user->clave = $request->getParsedBody()['clave'] ?? '';
+    //     $user->email = $request->getParsedBody()['email'] ?? '';
+    //     $user->tipo = $request->getParsedBody()['tipo'] ?? '';
+    
+    //     $rta = $user->save();
+    //     $response->getBody()->write( json_encode( $rta ) );
+        
+    //     return $response;
+        
+        
+    // }
+
+    // public function deleteUser ( Request $request, Response $response, $args ) {
+    
+    //     $idUrl = $args['id'] ?? '';
+    //     $user = Usuario::find( intval( $idUrl ) );
+    //     echo json_encode($user);
+
+
+    //     // $rta = $user->delete();
+    //     // $response->getBody()->write( json_encode( $rta ) );
+        
+    //     return $response;
+        
+    // }
+    
+}
 
 use App\Controllers\MesaController;
 use Slim\Factory\AppFactory;
@@ -41,7 +209,7 @@ use Slim\Routing\RouteCollectorProxy;
 
 // Controllers
 // use Config\Database;
-use App\Controllers\UsuarioController;
+// use App\Controllers\UsuarioController;
 use App\Controllers\ProductoController;
 use App\Controllers\PedidoController;
 use App\Controllers\PreparacioneController;
