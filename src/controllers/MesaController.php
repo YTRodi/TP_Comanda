e12f7a6fefbfe23b9564723c802b0abc3b8c8257
YTRodi <yagorodi@gmail.com>
2020-11-29 03:56:26 -0300

    e

--- src/controllers/MesaController.php
+++ src/controllers/MesaController.php
@@ -1,6 +1,6 @@
 <?php 
 
-namespace App\Controllers;
+namespace App\Controllers\MesaController;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
